585f69083e9dd9d73c13be4829bd99e015d0da43
Francisco Vargas <fjvarg4934@gmail.com>
2018-11-08 08:21:12 -0600

    Init commit

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/pet-log';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-pet-log';
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'default';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const { PORT, DATABASE_URL } = require('./config');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(express.json());
+
+app.get('/api/*', (req, res) => {
+   res.json({ok: true});
+ });
+
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
+
+function runServer(databaseURL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseURL, {useNewUrlParser: true }, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err)
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
