0e6999898295224fffe7dfc9498b496cd5065a98
Francisco Vargas <fjvarg4934@gmail.com>
2018-11-17 08:50:43 -0600

    Added CORS

--- server.js
+++ server.js
@@ -17,11 +17,20 @@ const app = express();
 // Middleware
 app.use(morgan('common'));
 app.use(express.json());
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+// app.use(
+//   cors({
+//     origin: CLIENT_ORIGIN
+//   })
+// );
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 // Used when authenticating user login
 passport.use(localStrategy);
